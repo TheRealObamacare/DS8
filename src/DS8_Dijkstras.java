2c3bd20f51c4438f2b2cbfed740721ec6d558e9a
Kal <134562542+TheRealObamacare@users.noreply.github.com>
2025-01-29 08:05:46 -0600

    almost done

--- src/DS8_Dijkstras.java
+++ src/DS8_Dijkstras.java
@@ -1,29 +1,42 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class DS8_Dijkstras
 {
     public static int dijkstras_Weighted(String[] edges, String vertices, char start, char end)
     {
         int distance = 0;
-        DS8_Queue<String> Queue = new DS8_Queue<>();
-        Queue.offer(start + "0");
+        ArrayList<DS8_Weighted_Node> nodes = new ArrayList<>();
+        nodes.add(new DS8_Weighted_Node(start, distance));
         boolean[] visited = new boolean[edges.length];
-        while(!Queue.isEmpty())
+        for (String yay : edges)
         {
-            String cur = Queue.poll();
-            if(cur.charAt(cur.length() - 1) == end)
+            nodes.add(new DS8_Weighted_Node(yay.charAt(0), Integer.MAX_VALUE));
+        }
+        ArrayList<DS8_Weighted_Node> sorted = nodes;
+        Collections.sort(sorted);
+        while(!nodes.isEmpty())
+        {
+            DS8_Weighted_Node cur = nodes.removeFirst();
+            if (cur.getDistance() == Integer.MAX_VALUE)
             {
-                return Integer.parseInt(cur.substring(cur.length() - 1));
+                break;
+            }
+            if(cur.getLocation() == end)
+            {
+                return cur.getDistance();
             }
             for(int i = 0; i < edges.length; i++)
             {
-                if (edges[i].charAt(0) == cur.charAt(cur.length() - 2) && !visited[i])
+                if (edges[i].charAt(0) == cur.getLocation() && !visited[i])
                 {
-                    cur = cur.substring(0, cur.length() - 1);
-                    distance += Integer.parseInt(cur.substring(cur.length() - 1));
-                    Queue.offer(cur + edges[i].charAt(1) + String.valueOf(distance));
+                    distance += Integer.parseInt(edges[i].substring(2));
+                    nodes.add(new DS8_Weighted_Node(edges[i].charAt(1), distance));
                     visited[i] = true;
                 }
             }
         }
         return -1;
     }
+
 }
