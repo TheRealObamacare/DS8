a1c9c4079bece6f87bea1d07acb48d4caa53f210
Kal <134562542+TheRealObamacare@users.noreply.github.com>
2025-02-03 07:54:20 -0600

    i HATE aStar

--- src/DS8_AStar.java
+++ src/DS8_AStar.java
@@ -13,17 +13,49 @@ public class DS8_AStar
         open.add(start);
         while(!open.isEmpty())
         {
-            Collections.sort(open -> open);
+            open.sort(DS8_AStar_Node::compareTo);
             DS8_AStar_Node cur = open.remove(0);
             if (cur.getLocation().equals('E'))
             {
                 return new DS8_Path_Solution(cur.getPath(), cur.getG());
             }
             closed.add(cur);
-            addToList(maze, );
-            addToList(maze, );
-            addToList(maze, );
-            addToList(maze, );
+            Point p = findChar(maze, cur.getLocation());
+            addToList(maze, open, closed, cur,  p.x+ 1, p.y);
+            addToList(maze, open, closed, cur, p.x - 1, p.y);
+            addToList(maze, open, closed, cur, p.x, p.y + 1);
+            addToList(maze, open, closed, cur, p.x, p.y - 1);
         }
+    }
+    public static void addToList(char[][] maze, ArrayList<DS8_AStar_Node> open, ArrayList<DS8_AStar_Node> closed, DS8_AStar_Node cur, int x, int y)
+    {
+        try
+        {
+            if(!closed.contains(maze[x][y]) && maze[x][y] != 'W')
+            {
+                if (open.contains(maze[x][y]))
+                {
+
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            return;
+        }
+    }
+    public static Point findChar(char[][] maze, char a)
+    {
+        for(int r = 0; r < maze.length; r++)
+        {
+            for(int c = 0; c < maze[0].length; c++)
+            {
+                if(maze[r][c] == a)
+                {
+                    return new Point(r, c);
+                }
+            }
+        }
+        return null;
     }
 }
